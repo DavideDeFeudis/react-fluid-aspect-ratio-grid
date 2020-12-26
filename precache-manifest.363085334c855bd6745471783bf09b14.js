cec71a19ac87599e57cd0d086f9e1cf120c106f0
Davide <davide@Felixs-iMac.local>
2020-12-26 13:57:11 +0100

    Updates

---
+++ precache-manifest.363085334c855bd6745471783bf09b14.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4429e0fb3a011604566c5fb2d61cbcce",
+    "url": "/react-fluid-aspect-ratio-grid/index.html"
+  },
+  {
+    "revision": "5ce95d83465eff7b3db2",
+    "url": "/react-fluid-aspect-ratio-grid/static/css/main.4411f538.chunk.css"
+  },
+  {
+    "revision": "18422179eddfc34d03ed",
+    "url": "/react-fluid-aspect-ratio-grid/static/js/2.0b42bec6.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/react-fluid-aspect-ratio-grid/static/js/2.0b42bec6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ce95d83465eff7b3db2",
+    "url": "/react-fluid-aspect-ratio-grid/static/js/main.79aa43d0.chunk.js"
+  },
+  {
+    "revision": "96d0e348eaf921c7aa21",
+    "url": "/react-fluid-aspect-ratio-grid/static/js/runtime-main.787ee127.js"
+  }
+]);
